a4d0004958b8916247ec43331653bfbac64afcdf
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-10-22 19:07:01 +0200

    Add ClapTrap private variables, constructor and destructor

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,1 +1,9 @@
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : _attackDamage(0), _energyPoint(10), _hitPoints(10), _name(name) {
+	std::cerr << _name << " CallTrap constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cerr << _name << " CallTrap destructor called" << std::endl;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,11 +1,17 @@
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <iostream>
+#include <string>
+
 class ClapTrap {
 private:
-
+	int _attackDamage;
+	int _energyPoint;
+	int _hitPoints;
+	std::string _name;
 public:
-	ClapTrap();
+	ClapTrap(std::string name);
 	~ClapTrap();
 };
 
