afb365d8b19eee7531fc27f264b49da41bdaa8f1
Dorian Eydoux <bonjour@dorian.cool>
2024-10-23 00:16:13 +0200

    Add ClapTrap member functions

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,9 +1,44 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(std::string name) : _attackDamage(0), _energyPoint(10), _hitPoints(10), _name(name) {
+ClapTrap::ClapTrap(std::string name) : _attackDamage(0), _energyPoints(10), _hitPoints(10), _name(name) {
 	std::cerr << _name << " CallTrap constructor called" << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
 	std::cerr << _name << " CallTrap destructor called" << std::endl;
+}
+
+void ClapTrap::attack(const std::string &target) {
+	if (_hitPoints <= 0)
+		std::cout << "ClapTrap " << _name << " is too damaged to attack!" << std::endl;
+	else if (_energyPoints <= 0)
+		std::cout << "ClapTrap " << _name << " has no energy points left to attack!" << std::endl;
+	else {
+		std::cout << "ClapTrap " << _name << " attacks " << target << " causing " << _attackDamage << " points of damage!" << std::endl;
+		_energyPoints--;
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	if (_hitPoints <= 0)
+		std::cout << "ClapTrap " << _name << " is already down!" << std::endl;
+	else if (amount >= _hitPoints) {
+		_hitPoints = 0;
+		std::cout << "ClapTrap " << _name << " takes " << amount << " damage points and is destroyed!" << std::endl;
+	} else {
+		_hitPoints -= amount;
+		std::cout << "ClapTrap " << _name << " takes " << amount << " damage points!" << std::endl;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	if (_hitPoints <= 0)
+		std::cout << "ClapTrap " << _name << " is too damaged for repairs!" << std::endl;
+	else if (_energyPoints <= 0)
+		std::cout << "ClapTrap " << _name << " has no energy points left for repairs!" << std::endl;
+	else {
+		_energyPoints--;
+		_hitPoints += amount;
+		std::cout << "ClapTrap " << _name << " repaired " << amount << " points!" << std::endl;
+	}
 }
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -7,12 +7,15 @@
 class ClapTrap {
 private:
 	int _attackDamage;
-	int _energyPoint;
+	int _energyPoints;
 	int _hitPoints;
 	std::string _name;
 public:
 	ClapTrap(std::string name);
 	~ClapTrap();
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 };
 
 #endif
