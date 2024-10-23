0329fec9f9fabb720d1333ef8a08b00c6d63233d
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-10-23 13:16:51 +0200

    Add ex00 main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,14 @@
 #include "ClapTrap.hpp"
 
 int main() {
+	ClapTrap jack("Jack");
 
+	for (size_t i = 0; i < 6; i++) {
+		jack.attack("Joe");
+		jack.beRepaired(1);
+	}
+
+	jack.takeDamage(10);
+	jack.takeDamage(5);
+	jack.takeDamage(1);
 }
